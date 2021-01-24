92489a8a2a4967fb844d37e6c63b10dc3cc4a235
Uko Akanowo <ukoakanowo98@gmail.com>
2021-01-24 23:47:48 +0100

    added client

---
+++ client/src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import App from "./components/App";
+import reducers from "../src/redux/reducers";
+
+import "./index.css";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+render(
+  <Provider
+    store={createStore(reducers, composeEnhancers(applyMiddleware(thunk)))}
+  >
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
