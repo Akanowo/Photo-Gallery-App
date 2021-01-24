bb112dd3e293dd55c24d57457aac2473fe1cc475
kevinvantus <kevinvantus@gmail.com>
2021-01-24 23:29:50 +0100

    Client-side commit

--- server/app.js
+++ server/app.js
@@ -1,37 +1,41 @@
 // Declarations
-const express = require('express');
-const mongoose = require('mongoose');
-const photoRouter = require('./routes/image.route');
+const express = require("express");
+const mongoose = require("mongoose");
+const photoRouter = require("./routes/image.route");
 
 // initialize app
 const app = express();
 
 // mongoDB configuration
-mongoose.connect('mongodb://localhost:27017/PhotoGalleryDB', { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
-  if(err) {
-    console.log('Could not connect to database: ', err.message);
-    return;
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/PhotoGalleryDB",
+  { useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log("Could not connect to database: ", err.message);
+      return;
+    }
+    console.log("DB connected successfully");
   }
-  console.log('DB connected successfully');
-});
+);
 
 // App config
 app.use(express.json()); // allow json formats
 app.use(express.urlencoded({ extended: false })); // allow url-form-encoded formats
-app.use('/api', photoRouter());
+app.use("/api", photoRouter());
 
 // routes configuration
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // Send response
-  res.send('You have reached the home endpoint');
+  res.send("You have reached the home endpoint");
 });
 
 // Handle wildcard routes
-app.get('**', (req, res) => {
-  res.redirect('/');
+app.get("**", (req, res) => {
+  res.redirect("/");
 });
 
 // set server to run on port 8080
 app.listen(8080, () => {
-  console.log('App started on port 8080');
+  console.log("App started on port 8080");
 });
